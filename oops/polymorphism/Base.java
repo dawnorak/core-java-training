1e4dd137f1b2e99bc2b1560cc966da1f95e297a8
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-01-11 13:25:26 +0530

    commit polymorphism example

---
+++ oops/polymorphism/Base.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Base {
+	
+	String name;
+	
+	public Base(String name) {
+		this.name = name;
+	}
+	
+	public void generateWelcome() {
+		System.out.println("No Welcome Yet!");
+	}
+}
