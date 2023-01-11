d7b01c9f142e38382acaf4e762f071a280af80a6
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-01-11 13:03:03 +0530

    Create StudentMarksApp.java

---
+++ oops/inheritance/StudentMarksApp.java
@@ -0,0 +1,9 @@
+package oops;
+
+public class StudentMarksApp {
+  
+  public static void main(String[] args){
+    CollegeStudent c1 = new CollegeStudent("Dawn", 20008, 9.2);
+    c1.generateMarksheet();
+  }
+}                       
