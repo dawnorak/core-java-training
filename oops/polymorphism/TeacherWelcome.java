8094bd2b40142aec0ab01728ab674814e146e6c0
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-01-11 13:23:35 +0530

    polymorphism example

---
+++ oops/polymorphism/TeacherWelcome.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class TeacherWelcome extends Base{
+	
+	public TeacherWelcome(String name) {
+		super(name);
+	}
+	
+	public void generateWelcome() {
+		System.out.println("Hello Teacher!");
+	}
+}
