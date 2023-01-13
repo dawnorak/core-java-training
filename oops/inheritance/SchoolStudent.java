07393f839ebb2cf408ef2f44a57dcbd94a72ee74
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-01-13 19:26:22 +0530

    add example

---
+++ oops/inheritance/SchoolStudent.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class SchoolStudent extends Student {
+	private double mark;
+	
+	public SchoolStudent(String name, int stCode, double mark) {
+		super(name, stCode);
+		this.mark = mark;
+	}
+	
+	public double getMark() {
+		return mark;
+	}
+	
+	public void setMark(double mark) {
+		this.mark = mark;
+	}
+	
+	public void generateMarksheet() {
+		System.out.println("Semester Detail");
+		System.out.println("Student Code: "+getStCode());
+		System.out.println("For: "+getName());
+		System.out.println("Marks: "+ getMark());
+	}
+}
