67b5057c9cd276b79b3613d402384973299240a8
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-01-13 19:27:28 +0530

    update

--- oops/inheritance/StudentMarksApp.java
+++ oops/inheritance/StudentMarksApp.java
@@ -4,6 +4,8 @@ public class StudentMarksApp {
   
   public static void main(String[] args){
     CollegeStudent c1 = new CollegeStudent("Dawn", 20008, 9.2);
+    SchoolStudent s1 = new SchoolStudent("Vaigarai", 28, 89);
     c1.generateMarksheet();
+    s1.generateMarksheet();
   }
 }                       
