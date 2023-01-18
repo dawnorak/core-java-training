833dff1fa1a25f22d283b628dceaad9fdea738e2
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-01-18 15:57:13 +0530

    Create Detention.java

---
+++ core/module1/loops/workbook 5.2/Detention.java
@@ -0,0 +1,11 @@
+public class Detention {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?\n");
+        String line = scan.nextLine();
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + ". " + line);
+        }
+        scan.close();
+    }
+}
